863e2529dd7c663424b2c236c84dff45d4bbfe7b
Phil Chen <06fahchen@gmail.com>
2022-06-23 18:12:27 +0800

    chore: add commanded as optional dep

--- mix.exs
+++ mix.exs
@@ -7,9 +7,7 @@ defmodule JetEventStore.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      dialyzer: [
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
-      ],
+      dialyzer: dialyzer(),
       aliases: aliases(),
       deps: deps()
     ]
@@ -25,6 +23,7 @@ defmodule JetEventStore.MixProject do
     [
       {:ecto, "~> 3.8"},
       {:typed_struct, "~> 0.3.0"},
+      {:commanded, "~> 1.3.1", optional: true},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
@@ -33,6 +32,14 @@ defmodule JetEventStore.MixProject do
   defp aliases do
     [
       "code.check": ["format --check-formatted", "credo --strict", "dialyzer"]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_add_apps: [:commanded],
+      plt_add_deps: :app_tree,
+      plt_file: {:no_warn, "priv/plts/jet_event_store.plt"}
     ]
   end
 end
